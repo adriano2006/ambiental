5878570dd3a508b648a5f6f97ce209018cf136fc
adriano2006 <132079505+adriano2006@users.noreply.github.com>
2024-06-17 09:05:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado:"Qual é um dos principais fontes de poluição do ar ? ",
         alternativas: [
             "Isso é assustador!",
             "Isso é maravilhoso!"
